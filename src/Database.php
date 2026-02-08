564e9d46ff80fd5a00801acce68352b40b8bf1d1
Yakiro-jin <cesarjozediazr@gmail.com>
2026-02-08 18:28:06 -0400

    coo

--- src/Database.php
+++ src/Database.php
@@ -2,23 +2,25 @@
 
 class Database {
     private $host;
+    private $port;
     private $db_name;
     private $username;
     private $password;
     public $conn;
 
     public function __construct() {
-        $this->host = getenv('DB_HOST') ?: 'db';
-        $this->db_name = getenv('POSTGRES_DB') ?: 'inventory';
-        $this->username = getenv('POSTGRES_USER') ?: 'user';
-        $this->password = getenv('POSTGRES_PASSWORD') ?: 'password';
+        $this->host = getenv('DB_HOST') ?: '192.168.100.100';
+        $this->port = getenv('DB_PORT') ?: '5432';
+        $this->db_name = getenv('DB_NAME') ?: 'inventory';
+        $this->username = getenv('DB_USER') ?: 'postgres';
+        $this->password = getenv('DB_PASSWORD') ?: 'password';
     }
 
     public function getConnection() {
         $this->conn = null;
 
         try {
-            $dsn = "pgsql:host=" . $this->host . ";dbname=" . $this->db_name;
+            $dsn = "pgsql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name;
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
